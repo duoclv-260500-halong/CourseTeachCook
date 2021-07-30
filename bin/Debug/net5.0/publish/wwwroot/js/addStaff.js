affec1de723f3c1e6fefcfbe1353ced97fb451b5
Duoc <duoclv.260500.halong@gmail.com>
2021-07-30 16:36:47 +0700

    30/07

--- bin/Debug/net5.0/publish/wwwroot/js/addStaff.js
+++
@@ -1,8 +0,0 @@
-function checkFormAddStaff(){
-    var name = document.getElementById("name1").value;
-    if(name == ''){
-        document.getElementById("errorName").innerHTML = "Tên không được trống";
-        return false;
-    }
-    return true;
-}
--- wwwroot/js/admin/viewStaff.js
+++
@@ -1,7 +0,0 @@
-function checkKey(){
-    var key = document.getElementById("key").value;
-    if(key == null || key == ""){
-        return false;
-    }
-    return true;
-}
